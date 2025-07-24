1304cdeae35c4b305285cb4da46b636af445ac86
Debalina Laha <32841776+Debalina99@users.noreply.github.com>
2025-07-24 19:45:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+let button = document.getElementById("btn");
+let output = document.getElementById("output");
+
+button.addEventListener("click", async () => {
+  let text = document.getElementById("text").value;
+  let delay = document.getElementById("delay").value;
+
+  if (!text || !delay) {
+    alert("Fill both fields!");
+    return;
+  }
+
+  delay = parseInt(delay) * 1000;
+	await new Promise(resolve => setTimeout(resolve, delay));
+    output.textContent = text;
+
+});
